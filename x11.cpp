116ed9c7a324cb996020455e16cedc5c0750f915
netpipe <netpipe@MSI.lan>
2021-11-26 08:58:14 -0700

    test

--- x11.cpp
+++ x11.cpp
@@ -14,11 +14,14 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+
 int screen;
 Display *display;
 XSetWindowAttributes window_attributes;
 Window mainwindow;
 XEvent events;
+    int root_x, root_y; //<--two
+    unsigned int mask; //<--three
 
 int main(int argc, char** argv) {
     display = XOpenDisplay(NULL);screen = DefaultScreen(display);
@@ -37,15 +40,54 @@ int main(int argc, char** argv) {
                              CWBackPixel|CWBorderPixel,
                              &window_attributes
                             );
+
+
+    XGrabPointer(display, mainwindow, False, ButtonPressMask, GrabModeAsync,
+         GrabModeAsync, None, None, CurrentTime);
+
+int button;
+int x=-1,y=-1;
     XMapWindow(display, mainwindow);
     XFlush(display);
     XSelectInput(display, mainwindow, ExposureMask|KeyPressMask|ButtonPressMask);
     while (1)  {
         XNextEvent(display, &events);
-        switch  (events.type) {
-            case Expose:
-                // Lots of code that doesn't matter for my problem
-            break;
+        //switch  (events.type) {
+
+ //   XQueryPointer(display, DefaultRootWindow(display), &mainwindow, &mainwindow, &root_x, &root_y, &root_x, &root_y, &mask); //<--four
+   // printf("Mouse coordinates (X: %d, Y: %d)\n", root_x, root_y);
+
+
+	switch(events.type){
+	    case ButtonPress:
+		switch(events.xbutton.button){
+		    case Button1:
+		        x=events.xbutton.x;
+		        y=events.xbutton.y;
+		        button=Button1;
+			printf("button1\n");
+		        break;
+
+		    case Button3:
+		        x=events.xbutton.x;
+		        y=events.xbutton.y;
+		        button=Button3;
+			printf("button3\n");
+		        break;
+		    default:
+		        break;
+		}
+	}
+
+        if (events.type == KeyPress)
+        {
+            printf( "KeyPress: %x\n", events.xkey.keycode );
+            if ( events.xkey.keycode == 0x09 )
+                break;
+        }
+        else if (events.type == KeyRelease)
+        {
+            printf( "KeyRelease: %x\n", events.xkey.keycode );
         }
     }
     return 0;
